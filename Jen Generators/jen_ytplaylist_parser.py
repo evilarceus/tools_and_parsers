8c2a7be325eed7f1ac0adf812e71788555c2d3df
evilarceus <evilarceus@gmail.com>
2018-03-31 17:29:14 -0700

    Added support for Python 3 for Jen Generators

--- Jen Generators/jen_ytplaylist_parser.py
+++ Jen Generators/jen_ytplaylist_parser.py
@@ -1,7 +1,6 @@
 # -*- coding: UTF-8 -*-
-""" Processes HTML of a YT channel's Playlists, to build a template file for all of them. """
 
-import os, re, unicodedata
+import os, re, unicodedata, chardet
 import dom_parser
 
 class Generator:
@@ -9,7 +8,7 @@ class Generator:
         # create initial variables needed later
 
         # CHANGEME Set this to the output filename you want/need.
-        self.htmlFilename = 'test.txt'
+        self.htmlFilename = "test.txt"
 
         # CHANGEME to match the path needed for your generic fanart to add to each entry. Set to None to not set it.
         # Example: self.fanart = None
@@ -21,20 +20,22 @@ class Generator:
         self._generate_yt_templates()
 
         # notify user
-        print "Finished parsing the html. Output saved as output_html.txt"
+        print("Finished parsing the html. Output saved as output_html.txt")
 
     def _generate_yt_templates ( self ):
 
-        print "Reading HTML File....."
+        print("Reading HTML File.....")
+        _path = self.tools_path + os.path.sep + self.htmlFilename
+        print(_path)
+        fileEncoding = chardet.detect(open(_path, "rb").read())["encoding"]
 
         try:
             output_string = ''
-            _path = self.tools_path + os.path.sep + self.htmlFilename   
-            r = open(_path)
-            html = r.read()       
-
+            
+            r = open(_path, encoding=fileEncoding)
+            html = r.read()
             the_divs = parseDOM(html, 'div', attrs={'class':'feed-item-dismissable '})
-            print 'Total Playlists: ' + str(len(the_divs))
+            print('Total Playlists: ' + str(len(the_divs)))
             for the_div in the_divs:
                 header = parseDOM(the_div, 'h3', attrs={'class':'yt-lockup-title '})[0]
                 link = parseDOM(header, 'a', attrs={'class':'yt-uix-sessionlink.+?'}, ret='href')[0]
@@ -57,16 +58,13 @@ class Generator:
                     output_string = output_string + '    <fanart>' + str(self.fanart) + '</fanart>\n'
                 output_string = output_string + '</plugin>\n\n'
 
-        except Exception, e:
-            # missing or poorly formatted xml
-            print "What the fuck dude? %s" % ( e )
-
-        # save file
-        with open("output_html.txt","w") as f:
-            f.write(output_string)
+        finally:
+            # save file
+            with open("output_html.txt", "w", encoding=fileEncoding) as f:
+                f.write(output_string)
 
 def parseDOM(html, name='', attrs=None, ret=False):
-    if attrs: attrs = dict((key, re.compile(value + ('$' if value else ''))) for key, value in attrs.iteritems())
+    if attrs: attrs = dict((key, re.compile(value + ('$' if value else ''))) for key, value in attrs.items())
     results = dom_parser.parse_dom(html, name, attrs, ret)
     if ret:
         results = [result.attrs[ret.lower()] for result in results]
@@ -76,16 +74,16 @@ def parseDOM(html, name='', attrs=None, ret=False):
 
 def replaceHTMLCodes(txt):
     txt = re.sub("(&#[0-9]+)([^;^0-9]+)", "\\1;\\2", txt)
-    import HTMLParser
-    txt = HTMLParser.HTMLParser().unescape(txt)
+    import html.parser
+    txt = html.parser.HTMLParser().unescape(txt)
     txt = txt.replace("&quot;", "\"")
     txt = txt.replace("&amp;", "&")
     txt = txt.strip()
     return txt
 
 def replaceEscapeCodes(txt):
-    import HTMLParser
-    txt = HTMLParser.HTMLParser().unescape(txt)
+    import html.parser
+    txt = html.parser.HTMLParser().unescape(txt)
     return txt
 
 if ( __name__ == "__main__" ):
